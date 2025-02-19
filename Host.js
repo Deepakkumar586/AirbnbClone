5dfd145518564d2fd20dac382933ff089aaab3d8
Deepak <aryanrajj1234@gmail.com>
2025-02-19 11:32:58 +0530

    update code with map

--- Host.js
+++ Host.js
@@ -14,3 +14,45 @@ function showSliderValue() {
 showSliderValue();
 window.addEventListener("resize",showSliderValue);
 slider_input.addEventListener('input', showSliderValue, false);
+
+
+// collapse
+const collapses = document.querySelectorAll("[app-collapse-header-btn]");
+
+const handleInitCollapse = (btnElement, parentEle) => {
+  const collapses = parentEle.querySelectorAll("[app-collapse-header-btn]");
+  collapses.forEach(collapse => {
+    const isIndipendente =  collapse.getAttribute('indipendente') === 'true';
+    const controlsId = collapse.getAttribute('aria-controls');
+    const contentEl = document.getElementById(controlsId);
+    
+    if(!isIndipendente && collapse !== btnElement){
+      collapse.setAttribute('aria-expanded', false);
+      contentEl.classList.remove('app-collapse-expanded');
+    }
+  })
+}
+
+const handleExpandToggle = (event) => { 
+  // console.log("handle Expand Toggle test log");
+  const btnElement = event.currentTarget;
+  const controlsId = btnElement.getAttribute('aria-controls');
+  const contentEl = document.getElementById(controlsId);
+  const parentEle = btnElement.closest("[app-accordian]");
+  const indipendente = btnElement.getAttribute('indipendente') === 'true';
+  
+  if(!indipendente && parentEle){
+    handleInitCollapse(btnElement, parentEle);
+  }
+
+  let open = btnElement.getAttribute('aria-expanded') === 'true';
+  if (open) {
+    btnElement.setAttribute('aria-expanded', `${!open}`);
+    contentEl.classList.remove('app-collapse-expanded');
+  } else {
+    btnElement.setAttribute('aria-expanded', `${!open}`);
+    contentEl.classList.add('app-collapse-expanded');
+  }
+
+} 
+collapses.forEach(collapse => collapse.addEventListener('click', handleExpandToggle));
